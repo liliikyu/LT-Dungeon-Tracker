6dc966f24fd09121f2587ae4f841c42803dd4883
liliikyu <ldieuvu@gmail.com>
2026-09-15 18:26:45 +0100

    Add files via upload

---
+++ assets/scenario-data.js
@@ -0,0 +1,821 @@
+window.LT_SCENARIO_DATA = {
+  "updated": "2026-09-15",
+  "source": {
+    "main": "https://latale.wiki.gg/wiki/Main_Scenario",
+    "sub": "https://latale.wiki.gg/wiki/Sub_Scenario"
+  },
+  "dungeons": {
+    "Dragon Lair": {
+      "main": [
+        "Ch.1-1"
+      ],
+      "sub": []
+    },
+    "Shangri-la": {
+      "main": [
+        "Ch.1-2"
+      ],
+      "sub": []
+    },
+    "Dark Moon of the 16th": {
+      "main": [
+        "Ch.1-2"
+      ],
+      "sub": [
+        "Ch.3-12"
+      ]
+    },
+    "Treasure Vault": {
+      "main": [
+        "Ch.1-2"
+      ],
+      "sub": []
+    },
+    "Gothic Room": {
+      "main": [
+        "Ch.1-2"
+      ],
+      "sub": []
+    },
+    "Captain Johnny's Room": {
+      "main": [],
+      "sub": []
+    },
+    "Rice Zone": {
+      "main": [],
+      "sub": []
+    },
+    "Heart of Ktuka": {
+      "main": [
+        "Ch.1-2"
+      ],
+      "sub": []
+    },
+    "Cold Heart": {
+      "main": [
+        "Ch.1-2"
+      ],
+      "sub": []
+    },
+    "Chimera Laboratory": {
+      "main": [
+        "Ch.1-3"
+      ],
+      "sub": []
+    },
+    "Frozen Labyrinth": {
+      "main": [],
+      "sub": []
+    },
+    "Toad Forest": {
+      "main": [],
+      "sub": []
+    },
+    "Toad Mountain": {
+      "main": [],
+      "sub": []
+    },
+    "Toad Cave": {
+      "main": [],
+      "sub": []
+    },
+    "Toad Valley": {
+      "main": [],
+      "sub": []
+    },
+    "King's Aircraft": {
+      "main": [],
+      "sub": []
+    },
+    "Valhalla": {
+      "main": [
+        "Ch.1-3"
+      ],
+      "sub": []
+    },
+    "Webfoot Octopus Boss Room": {
+      "main": [],
+      "sub": []
+    },
+    "Hell's Door": {
+      "main": [
+        "Ch.1-4"
+      ],
+      "sub": []
+    },
+    "Royal Rumble": {
+      "main": [],
+      "sub": []
+    },
+    "TAID Flower Valley": {
+      "main": [],
+      "sub": []
+    },
+    "Fairy Castle": {
+      "main": [],
+      "sub": []
+    },
+    "Outer Realm": {
+      "main": [
+        "Ch.1-5",
+        "Ch.1-6"
+      ],
+      "sub": [
+        "Ch.3-36"
+      ]
+    },
+    "Black Tower": {
+      "main": [],
+      "sub": []
+    },
+    "Constellation Cliff": {
+      "main": [],
+      "sub": [
+        "Ch.1-2"
+      ]
+    },
+    "TAID Empyrean": {
+      "main": [],
+      "sub": []
+    },
+    "TAID Frozen Labyrinth": {
+      "main": [],
+      "sub": []
+    },
+    "Magic Book Room": {
+      "main": [],
+      "sub": []
+    },
+    "Forgotten Garden": {
+      "main": [],
+      "sub": []
+    },
+    "Chunsik's Room": {
+      "main": [],
+      "sub": []
+    },
+    "Volcano": {
+      "main": [],
+      "sub": []
+    },
+    "Jiendia Warehouse": {
+      "main": [],
+      "sub": []
+    },
+    "Explorer's Hall": {
+      "main": [],
+      "sub": []
+    },
+    "White Plains": {
+      "main": [],
+      "sub": []
+    },
+    "Collapsed Tower Underground": {
+      "main": [
+        "Ch.1-5"
+      ],
+      "sub": []
+    },
+    "Star's Cradle": {
+      "main": [],
+      "sub": [
+        "Ch.1-2"
+      ]
+    },
+    "TAID Star's Cradle": {
+      "main": [],
+      "sub": []
+    },
+    "Ymir Institute": {
+      "main": [],
+      "sub": []
+    },
+    "Big Tube": {
+      "main": [],
+      "sub": []
+    },
+    "Road of Flower": {
+      "main": [
+        "Ch.1-6"
+      ],
+      "sub": []
+    },
+    "Road of Moon": {
+      "main": [
+        "Ch.1-6"
+      ],
+      "sub": []
+    },
+    "Road of Dream": {
+      "main": [
+        "Ch.1-6"
+      ],
+      "sub": []
+    },
+    "World's Genesis": {
+      "main": [],
+      "sub": []
+    },
+    "Crash Zone": {
+      "main": [],
+      "sub": [
+        "Ch.1-3"
+      ]
+    },
+    "Sky Coliseum": {
+      "main": [],
+      "sub": [
+        "Ch.2-1",
+        "Ch.3-8"
+      ]
+    },
+    "Mysterious Hall": {
+      "main": [],
+      "sub": [
+        "Ch.2-3"
+      ]
+    },
+    "Aurora Garden": {
+      "main": [],
+      "sub": [
+        "Ch.2-2",
+        "Ch.3-1",
+        "Ch.3-27"
+      ]
+    },
+    "TAID Dragon Garden": {
+      "main": [
+        "Ch.2-2"
+      ],
+      "sub": []
+    },
+    "Garden Of the Priring": {
+      "main": [],
+      "sub": []
+    },
+    "Rising Dragon Temple": {
+      "main": [
+        "Ch.2-2"
+      ],
+      "sub": [
+        "Ch.3-10"
+      ]
+    },
+    "Hidden Cave": {
+      "main": [
+        "Ch.2-3"
+      ],
+      "sub": []
+    },
+    "Rivera City Hall": {
+      "main": [],
+      "sub": []
+    },
+    "Ruined Palace": {
+      "main": [
+        "Ch.2-3"
+      ],
+      "sub": []
+    },
+    "Manastone Laboratory": {
+      "main": [],
+      "sub": [
+        "Ch.3-5",
+        "Ch.3-10"
+      ]
+    },
+    "TAID Choco Garden": {
+      "main": [],
+      "sub": []
+    },
+    "Devil's Canyon": {
+      "main": [],
+      "sub": [
+        "Ch.2-3"
+      ]
+    },
+    "Sacred Hall": {
+      "main": [
+        "Ch.2-5"
+      ],
+      "sub": []
+    },
+    "Twisted Genesis": {
+      "main": [],
+      "sub": [
+        "Ch.2-4"
+      ]
+    },
+    "Dwarf Aircraft": {
+      "main": [],
+      "sub": [
+        "Ch.2-5",
+        "Ch.3-1",
+        "Ch.3-10"
+      ]
+    },
+    "Sunset Forest": {
+      "main": [],
+      "sub": [
+        "Ch.2-7"
+      ]
+    },
+    "Solar Temple": {
+      "main": [
+        "Ch.2-6"
+      ],
+      "sub": [
+        "Ch.3-5"
+      ]
+    },
+    "Agni's Altar": {
+      "main": [
+        "Ch.2-6"
+      ],
+      "sub": []
+    },
+    "Chunsik Memorial": {
+      "main": [],
+      "sub": []
+    },
+    "Treasure Cave": {
+      "main": [],
+      "sub": [
+        "Ch.2-6",
+        "Ch.3-1"
+      ]
+    },
+    "Book of Shadows": {
+      "main": [],
+      "sub": [
+        "Ch.2-8"
+      ]
+    },
+    "Euphony of Corals": {
+      "main": [],
+      "sub": [
+        "Ch.2-9",
+        "Ch.3-17"
+      ]
+    },
+    "Waterfall Forest": {
+      "main": [
+        "Ch.2-7"
+      ],
+      "sub": []
+    },
+    "Jewell Forest": {
+      "main": [
+        "Ch.2-7"
+      ],
+      "sub": [
+        "Ch.3-6",
+        "Ch.3-25"
+      ]
+    },
+    "Ice Cream Garden": {
+      "main": [],
+      "sub": []
+    },
+    "Ra's Palace": {
+      "main": [
+        "Ch.3-1"
+      ],
+      "sub": [
+        "Ch.3-5"
+      ]
+    },
+    "Warrior's Graveyard": {
+      "main": [
+        "Ch.3-6"
+      ],
+      "sub": [
+        "Ch.3-16",
+        "Ch.3-17",
+        "Ch.3-55"
+      ]
+    },
+    "Zerenis Training Center": {
+      "main": [],
+      "sub": [
+        "Ch.4-1"
+      ]
+    },
+    "Floating Island": {
+      "main": [
+        "Ch.2-8"
+      ],
+      "sub": [
+        "Ch.3-2"
+      ]
+    },
+    "Illusion Mist Swamp": {
+      "main": [
+        "Ch.2-8"
+      ],
+      "sub": []
+    },
+    "Twin Caves": {
+      "main": [],
+      "sub": [
+        "Ch.2-9",
+        "Ch.3-2",
+        "Ch.3-4",
+        "Ch.3-13"
+      ]
+    },
+    "Canyon of Chaos": {
+      "main": [],
+      "sub": [
+        "Ch.3-7",
+        "Ch.3-29"
+      ]
+    },
+    "Frozen World": {
+      "main": [
+        "Ch.2-9"
+      ],
+      "sub": [
+        "Ch.3-2",
+        "Ch.3-5",
+        "Ch.3-7",
+        "Ch.3-11",
+        "Ch.3-12",
+        "Ch.3-18",
+        "Ch.3-27",
+        "Ch.3-37",
+        "Ch.3-55"
+      ]
+    },
+    "Hall of Rest": {
+      "main": [
+        "Ch.3-1"
+      ],
+      "sub": [
+        "Ch.3-4",
+        "Ch.3-5",
+        "Ch.3-15",
+        "Ch.3-18"
+      ]
+    },
+    "Champion's Memorial": {
+      "main": [
+        "Ch.3-2"
+      ],
+      "sub": [
+        "Ch.3-10",
+        "Ch.3-22"
+      ]
+    },
+    "Chamber of Hell": {
+      "main": [
+        "Ch.3-2"
+      ],
+      "sub": [
+        "Ch.3-6",
+        "Ch.3-8",
+        "Ch.3-13",
+        "Ch.3-15",
+        "Ch.4-6"
+      ]
+    },
+    "Monster Tree Valley": {
+      "main": [
+        "Ch.3-3"
+      ],
+      "sub": [
+        "Ch.3-11",
+        "Ch.3-17",
+        "Ch.3-18"
+      ]
+    },
+    "Dragon Valley": {
+      "main": [
+        "Ch.3-4",
+        "Ch.3-8"
+      ],
+      "sub": [
+        "Ch.3-9",
+        "Ch.3-12",
+        "Ch.3-13"
+      ]
+    },
+    "Divine Tree Rapier": {
+      "main": [
+        "Ch.3-4"
+      ],
+      "sub": [
+        "Ch.3-20",
+        "Ch.3-22",
+        "Ch.3-25",
+        "Ch.3-50",
+        "Ch.3-55"
+      ]
+    },
+    "Zerenis Headquarters": {
+      "main": [
+        "Ch.3-5"
+      ],
+      "sub": [
+        "Ch.3-18",
+        "Ch.3-21",
+        "Ch.3-40",
+        "Ch.3-46"
+      ]
+    },
+    "Fallout Shelter": {
+      "main": [
+        "Ch.3-6"
+      ],
+      "sub": []
+    },
+    "Dream Oneiro": {
+      "main": [
+        "Ch.3-6"
+      ],
+      "sub": [
+        "Ch.3-20",
+        "Ch.3-21",
+        "Ch.3-23",
+        "Ch.3-51"
+      ]
+    },
+    "Void Star": {
+      "main": [],
+      "sub": [
+        "Ch.3-26"
+      ]
+    },
+    "Oblivon Lake": {
+      "main": [],
+      "sub": [
+        "Ch.3-27",
+        "Ch.3-32",
+        "Ch.3-34",
+        "Ch.3-49"
+      ]
+    },
+    "Flame's Cradle": {
+      "main": [
+        "Ch.3-8"
+      ],
+      "sub": [
+        "Ch.3-30",
+        "Ch.3-53"
+      ]
+    },
+    "Rosengarten": {
+      "main": [
+        "Ch.3-8"
+      ],
+      "sub": [
+        "Ch.3-29",
+        "Ch.3-31",
+        "Ch.3-53",
+        "Ch.4-6"
+      ]
+    },
+    "Hyle": {
+      "main": [
+        "Ch.3-9",
+        "Ch.4-1"
+      ],
+      "sub": [
+        "Ch.3-35"
+      ]
+    },
+    "Promised Sancutary": {
+      "main": [
+        "Ch.3-9"
+      ],
+      "sub": [
+        "Ch.3-36"
+      ]
+    },
+    "Icicle Prison": {
+      "main": [
+        "Ch.3-10"
+      ],
+      "sub": [
+        "Ch.3-41",
+        "Ch.3-50",
+        "Ch.3-52"
+      ]
+    },
+    "Spring of the Echo": {
+      "main": [
+        "Ch.3-10"
+      ],
+      "sub": [
+        "Ch.3-38",
+        "Ch.3-39",
+        "Ch.3-41",
+        "Ch.3-45",
+        "Ch.3-52",
+        "Ch.3-55"
+      ]
+    },
+    "Heart of Reminiscience": {
+      "main": [
+        "Ch.3-11"
+      ],
+      "sub": [
+        "Ch.3-43"
+      ]
+    },
+    "Acro Coffin": {
+      "main": [
+        "Ch.3-11"
+      ],
+      "sub": [
+        "Ch.3-44"
+      ]
+    },
+    "Vanitas": {
+      "main": [
+        "Ch.3-12"
+      ],
+      "sub": [
+        "Ch.3-51"
+      ]
+    },
+    "Purgatory Azreal": {
+      "main": [
+        "Ch.3-12"
+      ],
+      "sub": [
+        "Ch.3-51"
+      ]
+    },
+    "Chronos' Time": {
+      "main": [
+        "Ch.3-13"
+      ],
+      "sub": [
+        "Ch.3-56"
+      ]
+    },
+    "Kairos' Time": {
+      "main": [
+        "Ch.3-13"
+      ],
+      "sub": [
+        "Ch.3-57",
+        "Ch.4-6"
+      ]
+    },
+    "Unknown Forest": {
+      "main": [],
+      "sub": []
+    },
+    "Chaotic Mirror": {
+      "main": [
+        "Ch.4-1"
+      ],
+      "sub": [
+        "Ch.4-2",
+        "Ch.4-3"
+      ]
+    },
+    "Theater Eugamon": {
+      "main": [
+        "Ch.4-1"
+      ],
+      "sub": [
+        "Ch.4-3"
+      ]
+    },
+    "Entelechy of Life": {
+      "main": [],
+      "sub": [
+        "Ch.4-6"
+      ]
+    },
+    "Stump of Spirits": {
+      "main": [],
+      "sub": [
+        "Ch.4-6",
+        "Ch.4-8"
+      ]
+    },
+    "Euphony of the Vanished Star": {
+      "main": [
+        "Ch.4-3"
+      ],
+      "sub": [
+        "Ch.4-8"
+      ]
+    },
+    "Twilight Cathedral": {
+      "main": [
+        "Ch.4-3"
+      ],
+      "sub": [
+        "Ch.4-8"
+      ]
+    },
+    "Trial of the Dark Moon": {
+      "main": [],
+      "sub": []
+    },
+    "Trial of the Earth": {
+      "main": [],
+      "sub": []
+    },
+    "Aegir Ruins": {
+      "main": [],
+      "sub": []
+    },
+    "Atlas Garden": {
+      "main": [
+        "Ch.4-5",
+        "Ch.4-7"
+      ],
+      "sub": []
+    },
+    "Niflheim Station": {
+      "main": [],
+      "sub": []
+    },
+    "Muspelheim Tunnel": {
+      "main": [],
+      "sub": []
+    },
+    "Moksha": {
+      "main": [],
+      "sub": []
+    },
+    "Tears of Nornir": {
+      "main": [],
+      "sub": []
+    },
+    "Mushroom Swamp": {
+      "main": [],
+      "sub": []
+    },
+    "Gates of Lysithea": {
+      "main": [],
+      "sub": []
+    },
+    "Boundary of the Void": {
+      "main": [],
+      "sub": []
+    },
+    "Pleroma": {
+      "main": [],
+      "sub": []
+    },
+    "Eroded Arcane Hall": {
+      "main": [],
+      "sub": []
+    },
+    "Pseudaria": {
+      "main": [],
+      "sub": []
+    },
+    "Muchkin Storage": {
+      "main": [],
+      "sub": []
+    },
+    "Emeraldia": {
+      "main": [],
+      "sub": []
+    },
+    "Helepol Fort": {
+      "main": [],
+      "sub": []
+    },
+    "Trials of Iron": {
+      "main": [],
+      "sub": []
+    },
+    "Mare Ingenii": {
+      "main": [],
+      "sub": []
+    },
+    "Wings of Icarus": {
+      "main": [],
+      "sub": []
+    },
+    "Unknown Beach": {
+      "main": [],
+      "sub": []
+    },
+    "Inspirational Playground": {
+      "main": [],
+      "sub": []
+    },
+    "Nectar Circus Hall": {
+      "main": [],
+      "sub": []
+    },
+    "Nest of the Blind Bird": {
+      "main": [],
+      "sub": []
+    },
+    "Rikimo Pelke": {
+      "main": [],
+      "sub": []
+    }
+  }
+};

07a2abe6e8af8fff478f35a546ea11b2032d38a7
liliikyu <ldieuvu@gmail.com>
2026-09-15 17:27:54 +0100

    Add files via upload

---
+++ assets/scenario-data.js
@@ -0,0 +1,37 @@
+window.LT_SCENARIO_DATA = {
+  "updated": "2026-09-15",
+  "source": {
+    "main": "https://latale.wiki.gg/wiki/Main_Scenario",
+    "sub": "https://latale.wiki.gg/wiki/Sub_Scenario"
+  },
+  "dungeons": {
+    "Fallout Shelter": {
+      "main": [
+        "Ch.3-6"
+      ],
+      "sub": []
+    },
+    "Frozen World": {
+      "main": [
+        "Ch.2-9"
+      ],
+      "sub": [
+        "Ch.3-2",
+        "Ch.3-5",
+        "Ch.3-7",
+        "Ch.3-11",
+        "Ch.3-12",
+        "Ch.3-18",
+        "Ch.3-21",
+        "Ch.3-37",
+        "Ch.3-55"
+      ]
+    },
+    "Purgatory Azreal": {
+      "main": [],
+      "sub": [
+        "Ch.3-51"
+      ]
+    }
+  }
+};
